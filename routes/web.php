aa3dea96fa02f6a648e2ec40bddf9eefdcd4a6ed
JoseBieco <jo_bieco@hotmail.com>
2022-02-10 13:43:59 -0300

    feat(routes): author routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('authors', ['uses' => 'AuthorController@getAllAuthors']);
+
+    $router->get('authors/{id}', ['uses' => 'AuthorController@getAuthorById']);
+
+    $router->post('authors', ['uses' => 'AuthorController@create']);
+
+    $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
+
+    $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
+});
