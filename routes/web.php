74691199b511bfa27ebae4398daec7046235e021
JoseBieco <jo_bieco@hotmail.com>
2022-02-14 15:43:36 -0300

    feat(auth): grouped routes and add auth routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,24 @@ $router->get('/', function () use ($router) {
 
 
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('authors', ['uses' => 'AuthorController@getAllAuthors']);
-
-    $router->get('authors/{id}', ['uses' => 'AuthorController@getAuthorById']);
-
-    $router->post('authors', ['uses' => 'AuthorController@create']);
-
-    $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
-
-    $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
 
     $router->post('register', ['uses' => 'AuthController@register']);
 
+    $router->post('login', ['uses' => 'AuthController@login']);
+
     $router->group(['prefix' => 'users'], function () use ($router) {
         $router->get('/', ['uses' => 'UserController@getAllUsers']);
+    });
+
+    $router->group(['prefix' => 'authors', 'middleware' => 'auth'], function () use ($router) {
+        $router->get('/', ['uses' => 'AuthorController@getAllAuthors']);
+
+        $router->get('/{id}', ['uses' => 'AuthorController@getAuthorById']);
+
+        $router->post('/', ['uses' => 'AuthorController@create']);
+
+        $router->delete('/{id}', ['uses' => 'AuthorController@delete']);
+
+        $router->put('/{id}', ['uses' => 'AuthorController@update']);
     });
 });
